f48a621a4b72a79d390577bc3efcee9876ef374a
gayathridiya007-design <gayathridiya007@email.com>
2026-06-04 18:11:21 +0530

    ninth commit

---
+++ javascript/basic.js
@@ -0,0 +1,26 @@
+// console.log("Hello World!")
+// let name="Gayathri"
+//let name=prompt("What is your name?");
+//console.log("hello"+name+"!")
+let a=45
+let b=40
+c=a+b
+console.log("C",a+b)
+if(c>=100)
+    {
+     console.log("GradeA")
+}
+ else if(c>=80)
+    {
+     console.log("GradeB")
+}
+else if(c>=40)
+    {
+     console.log("GradeC")
+}
+else
+{
+    
+     console.log("GradeD")
+}
+
